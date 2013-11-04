24658c7125f43b59cd12fd0586bbfbe86e57b240
Carl P. Corliss <rabbitt@gmail.com>
2013-11-04 17:45:03 -0500

    handle different readlinks in init too

--- init.sh
+++ init.sh
@@ -1,15 +1,27 @@
-root_path=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+case "$(uname -s)" in
+  Darwin)
+    export readlink="$(ruby -rpathname -e "print Pathname.new('$0').realpath.dirname")/bin/ruby-readlink";;
+  Linux)
+    export readlink="$(which readlink)";;
+  *)
+    echo "Unsupported OS: $(uname -s)"
+    exit 1;;
+esac
+
+root_path=$(dirname $($readlink -f ${BASH_SOURCE[0]}))
 
 # preinit
 source  ${root_path}/pre-init
 
 # load environment
-for mod in env alias functions 3rdparty; do
-  for file in "${root_path}/${mod}.d"/*.${mod}; do
-    # echo "loading: ${file}"
-    source "${file}"
+for _mod in env alias functions 3rdparty; do
+  for _file in "${root_path}/${_mod}.d"/*.${_mod}; do
+    # echo "loading: ${_file}"
+    source "${_file}"
   done
 done
 
 # post-init
 source  ${root_path}/post-init
+
+unset root_paths _mod _file
